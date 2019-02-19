2033326a68216c6bff1fc83be11fad043dade87d
Allan Tony Selvan <allan.tony.selvan@nl.abnamro.com>
2019-02-19 20:56:15 +0100

    Added a script to add all groovy scripts to nexus

---
+++ src/com/privatesquare/go/nexus3-repository-cli/scripts/setup/add-or-update-all-scripts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CLI="./nexus3-repository-cli"
+scripts=("get-repo"
+         "create-hosted-repo"
+         "create-proxy-repo"
+         "create-group-repo"
+         "add-group-members"
+         "delete-repo"
+         "get-content-selectors"
+         "create-content-selector"
+         "update-content-selector"
+         "delete-content-selector"
+         "get-privileges"
+         "create-privilege"
+         "update-privilege"
+         "delete-privilege"
+         "get-roles"
+         "create-role"
+         "delete-role"
+        )
+
+for script in "${scripts[@]}"
+do
+    ${CLI} script -skip-tls -task add-or-update -name ${script}
+done
